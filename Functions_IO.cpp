013356ecb65a84c9d88e7a461db33c9b1f7bef38
Forever <turboromeo5@gmail.com>
2026-06-21 13:57:19 +0300

    Functions for input and output were implemented (loadExpressionFromFile and saveResultToFile)

--- Functions_IO.cpp
+++ Functions_IO.cpp
@@ -1,8 +1,46 @@
 #include "Functions_IO.h"
 
+//Метод считывания выражений с файла
 bool loadExpressionFromFile(const QString& path, QStringList& expressions, QList<Error>& errors) {
+    QFile file(path);
+
+    if (path.isEmpty() || !file.open(QIODevice::ReadOnly)) {
+        errors.append(Error(Error::FileNotExist, "", "", path));
+        return false;
+    }
+
+    QString fullContent = QString::fromUtf8(file.readAll()).trimmed();
+    file.close();
+
+    if (fullContent.isEmpty()) {
+        errors.append(Error(Error::EmptyInputFile, "", "", path));
+        return false;
+    }
+
+    expressions = fullContent.split('\n', Qt::SkipEmptyParts);
+
+    for (int i = 0; i < expressions.size(); ++i) {
+
+        if (expressions[i].trimmed().length() > 1000) {
+            errors.append(Error(Error::ExpressionTooLong, "", "", path));
+
+            printf("Error: Expression length exceeds 1000 characters in file %s (Line %d).\n",
+                   qPrintable(path), i + 1);
+
+            expressions.clear();
+            return false;
+        }
+    }
     return true;
 }
 
+//Метод записи перевода выражения в файл
 void saveResultToFile(const QString& text, const QString& filePath) {
+    QFile outFile(filePath);
+    //Перезаписывает содержимое существующего файла или создаёт новый, если файл не существует
+    if (outFile.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+        outFile.write(text.toUtf8());
+        outFile.close();
+        printf("\nResult saved to %s\n", qPrintable(filePath));
+    }
 }
